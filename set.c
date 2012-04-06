b5a13478c4aca42b8e1883f1e1647f605db0beeb
root <root@28d090b4-1bae-4c57-b5de-cc76a57c8abd>
2012-04-06 10:58:15 +0000

    kdfljsqnhdgubdnhsgnikd
    git-svn-id: file:///home/qsantos/A/svncompilo@48 28d090b4-1bae-4c57-b5de-cc76a57c8abd

--- set.c
+++ set.c
@@ -1,6 +1,6 @@
 #include "set.h"
 
-#incldue <stdlib.h>
+#include <stdlib.h>
 
 Set Empty_Set = NULL;
 
@@ -24,21 +24,57 @@ Set Set_Append(u32 a, Set s)
 		return Set_Singleton(a);
 	else if (s->head > a)
 	{
-		Set s = (Set) malloc(sizeof(struct Set));
-		assert(s);
-		s->head = a;
-		s->tail = s;
+		Set t = (Set) malloc(sizeof(struct Set));
+		assert(t);
+		t->head = a;
+		t->tail = s;
+		return t;
+	}
+	else if (s->head < a)
+	{
+		Set t = (Set) malloc(sizeof(struct Set));
+		assert(t);
+		t->head = s->head;
+		t->tail = Set_Append(a, s->tail);
+		return t;
+	}
+	else
+		return Set_Copy(s)
+}
+
+Set Set_Union(Set a, Set b)
+{
+	if (a == Empty_Set)
+	{
+		if (b == Empty_Set)
+			return Empty_Set;
+		else
+			return Set_Copy(b);
+	}
+	else
+	{
+		if (b == Empty_Set)
+			return Set_Copy(a);
+		else if (a->head < b->head)
+			return Set_Append(a->head, Set_Union(a->tail, b));
+		else if (a->head > b->head)
+			return Set_Append(b->head, Set_Union(a, b-tail));
+		else
+			return Set_Append(a->head, Set_Union(a->tail, b-tail));
 	}
 }
 
-Set Set_Union(Set, Set)
+Set Set_Diff(Set a, Set b)
 {
+	assert(false);
+	return a;
 }
 
-Set Set_Diff(Set, Set)
+void Set_Delete(Set s)
 {
-}
-
-void Set_Delete(Set)
-{
+	if (s != Empty_Set)
+	{
+		Set_Delete(s->tail);
+		free(s);
+	}
 }
--- set.h
+++ set.h
@@ -12,10 +12,10 @@ struct Set
 
 extern Set Empty_Set;
 
-Set  Set_Pair  (u32, u32);
-Set  Set_Append(u32, Set);
-Set  Set_Union (Set, Set);
-Set  Set_Diff  (Set, Set);
-void Set_Delete(Set);
+inline Set Set_Pair  (u32, u32);
+Set        Set_Append(u32, Set);
+Set        Set_Union (Set, Set);
+Set        Set_Diff  (Set, Set);
+void       Set_Delete(Set);
 
 #endif
